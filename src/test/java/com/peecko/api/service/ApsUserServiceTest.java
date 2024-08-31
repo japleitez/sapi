0ba814787f5cb6b972fbeec702a2d60315268f93
japleitez <jose.pleitez@gmail.com>
2024-08-31 13:38:00 +0200

    clean up services

--- src/test/java/com/peecko/api/service/ApsUserServiceTest.java
+++ src/test/java/com/peecko/api/service/ApsUserServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-@EntityScan(basePackages = "com.peecko.api.domain")
 class ApsUserServiceTest {
 
     @Autowired
