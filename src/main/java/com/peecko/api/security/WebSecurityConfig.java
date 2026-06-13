8223aacb7703b5fa7f636612662b7ea1b8760d4a
jose.pleitez <jose.pleitez@gmail.com>
2026-06-13 11:33:24 +0200

    prod log folder and health endpoint

--- src/main/java/com/peecko/api/security/WebSecurityConfig.java
+++ src/main/java/com/peecko/api/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
 
         http
             .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers("/api/auth/**", "/api/test/**").permitAll()
+                .requestMatchers("/api/auth/**", "/api/test/**", "/health").permitAll()
                 .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
