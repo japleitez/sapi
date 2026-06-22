6e1fd6e238abe4ab265f666763d64d1b205067c7
jose.pleitez <jose.pleitez@gmail.com>
2026-06-22 09:46:41 +0200

    set 1.0.2 version

--- src/main/java/com/peecko/api/security/WebSecurityConfig.java
+++ src/main/java/com/peecko/api/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
 
         http
             .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers("/api/auth/**", "/api/test/**", "/health/**", "/health").permitAll()
+                .requestMatchers("/api/auth/**", "/api/test/**", "/health", "/version").permitAll()
                 .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
--- src/main/java/com/peecko/api/web/rest/HealthResource.java
+++ src/main/java/com/peecko/api/web/rest/HealthResource.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
  * Used by AWS ALB to verify instance health
  */
 @RestController
-@RequestMapping("/health")
 public class HealthResource {
 
     private static final Logger log = LoggerFactory.getLogger(HealthResource.class);
@@ -28,7 +26,7 @@ public class HealthResource {
      *
      * @return ResponseEntity with status 200 and body "ok"
      */
-    @GetMapping
+    @GetMapping("/health")
     public ResponseEntity<String> health() {
         log.info("health");
         return ResponseEntity.status(HttpStatus.OK).body("ok");
